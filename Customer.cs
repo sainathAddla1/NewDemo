ee2f861266da0e22f456f18cc08fefa400ce9e04
sainathAddla1 <sainath93462@gmail.com>
2023-08-26 12:33:59 +0530

    MyClassFiles

---
+++ Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Customer
+{
+	public string Id { get; set; }
+	public string CustomerName { get; set; }
+	public string CustomerPhone { get; set; }
+	public string CustomerCity { get; set; }
+	public string CustomerRegion { get; set; }
+	public string CustomerPostalCode { get; set;
+	public string CustomerCountry { get; set; }
+}
+}
